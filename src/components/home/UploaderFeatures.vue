5153b4763543045a7bf290e7cdece20d11f11ce8
Amir Gafurov <gafurovamir@gmail.com>
2022-02-08 10:14:28 +0400

    преимущества на странице загрузки файлов

---
+++ src/components/home/UploaderFeatures.vue
@@ -0,0 +1,81 @@
+<template>
+  <section class="uploader-features">
+    <div class="uploader-features__item">
+      <svg-icon name="convert-pdf" />
+
+      <div class="text">
+        <span>Конвертировать файл</span>
+
+        <span>из .DOC, .DOCX, .JPG или .EXEL</span>
+      </div>
+    </div>
+
+    <div class="uploader-features__item">
+      <svg-icon name="merge-pdf" />
+
+      <div class="text">
+        <span>Объединить файлы</span>
+
+        <span>из нескольких .PDF в один</span>
+      </div>
+    </div>
+
+    <div class="uploader-features__item">
+      <svg-icon name="divide-pdf" />
+
+      <div class="text">
+        <span>Разделить файл</span>
+
+        <span>из одного .PDF в два или больше</span>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'UploaderFeatures',
+};
+</script>
+
+<style lang="scss">
+.uploader-features {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
+  gap: 30px;
+
+  &__item {
+    display: flex;
+    align-items: center;
+    border: 1px solid $color-border;
+    border-radius: $base-border-radius;
+    padding: 25px;
+
+    .svg-icon {
+      width: 86px;
+      height: 86px;
+      flex-shrink: 0;
+    }
+
+    .text {
+      padding-left: 20px;
+
+      span {
+        display: block;
+
+        &:first-child {
+          font-weight: 500;
+          font-size: 15px;
+          line-height: 17px;
+          margin-bottom: 15px;
+        }
+
+        &:last-child {
+          font-size: 13px;
+          line-height: 19px;
+        }
+      }
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,35 @@
 <template>
   <div class="home-page container">
     <file-uploader class="home-page__uploader" />
+
+    <uploader-features class="home-page__features" />
   </div>
 </template>
 
 <script>
 import FileUploader from '@/components/home/FileUploader.vue';
+import UploaderFeatures from '@/components/home/UploaderFeatures.vue';
 
 export default {
   name: 'HomePage',
 
-  components: { FileUploader },
+  components: { UploaderFeatures, FileUploader },
 };
 </script>
 
 <style lang="scss">
 .home-page {
+  display: flex;
+  flex-direction: column;
+
   &__uploader {
     padding-top: $base-gap * 2;
     margin-bottom: 170px;
+    flex-grow: 1;
+  }
+
+  &__features {
+    flex-shrink: 0;
   }
 }
 </style>
